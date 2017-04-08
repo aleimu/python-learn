71a29bd86ce86de835e21e2553d32485277b7c00
lgj <2086530297@qq.com>
2017-04-08 09:03:39 +0800

    Update ctypes使用.py

--- Example3/ctypes使用.py
+++ Example3/ctypes使用.py
@@ -1,9 +1,3 @@
-刚学python，前几天在java中调用了win32api，给eclipse窗口来了个抖动，也想拿python实现下。
-在网上找python调用win32api的资料，清一色的是win32api模块，我晕。
-作为一个新手，我也知道python调用c还是很方便的，我也不想去sourceforge上下载模块安装，于是翻遍了google，
-加上自己的“冷静思考，理性分析” 哈哈！！终于通过两种方法实现
-摘录在此，以备本人以及其他python新人查阅
-（我的环境是 python2.7）
 这里我们实现的是：枚举所有窗口，输出窗口的titile
 
 第一种，通过ctypes的windll,user32实现。
