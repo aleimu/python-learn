9e10ae80a980350da077da211c67e85e446e7fe3
lgj <2086530297@qq.com>
2017-05-19 17:19:19 +0800

    Update python学习总结.py

--- python学习总结.py
+++ python学习总结.py
@@ -2525,6 +2525,78 @@ ret_str = str(ret_bytes,encoding="utf-8")
 print(ret_str)
 }
 
+例子5{
+#!/usr/bin/env python
+# Version = 3.4
+#利用select实现伪同时处理多个Socket客户端请求读写分离
+import socket
+import select
+
+sk1 = socket.socket()
+sk1.bind(('127.0.0.1', 8001))
+sk1.listen(5)
+
+inputs = [sk1, ]
+outputs = []
+message_dict = {}
+
+while True:
+    r_list, w_list, e_list = select.select(inputs, outputs, inputs, 1)
+    print('正在监听的socket对象%d' % len(inputs))
+    print(r_list)
+    for sk1_or_conn in r_list:
+        #每一个连接对象
+        if sk1_or_conn == sk1:
+            # 表示有新用户来连接
+            conn, address = sk1_or_conn.accept()
+            inputs.append(conn)
+            message_dict[conn] = []
+        else:
+            # 有老用户发消息了
+            try:
+                data_bytes = sk1_or_conn.recv(1024)
+            except Exception as ex:
+                # 如果用户终止连接
+                inputs.remove(sk1_or_conn)
+            else:
+                data_str = str(data_bytes, encoding='utf-8')
+                message_dict[sk1_or_conn].append(data_str)
+                outputs.append(sk1_or_conn)
+
+    #w_list中仅仅保存了谁给我发过消息
+    for conn in w_list:
+        recv_str = message_dict[conn][0]
+        del message_dict[conn][0]
+        conn.sendall(bytes(recv_str+'好', encoding='utf-8'))
+        outputs.remove(conn)
+
+    for sk in e_list:
+        inputs.remove(sk)
+
+
+
+#!/usr/bin/env python
+# Version = 3.4
+import socket
+
+obj = socket.socket()
+
+obj.connect(('127.0.0.1',8001))
+
+while True:
+    inp = input("Please(q\退出):\n>>>")
+    obj.sendall(bytes(inp,encoding="utf-8"))
+    if inp == "q":
+        break
+    ret = str(obj.recv(1024),encoding="utf-8")
+    print(ret)
+		
+}
+
+例子6{
+
+}
+
 }
 
 #http://www.cnblogs.com/huyuedong/p/5882510.html  很不错的博客
