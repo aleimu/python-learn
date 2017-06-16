b857e72b14a8d1df627e95e0ddb2fa26aa655622
2086530297@qq.com <qwer@123>
2017-06-16 23:10:12 +0800

    解决方法

--- 问题记录.sh
+++ 问题记录.sh
@@ -17,3 +17,30 @@ Traceback (most recent call last):
     from socketserver import ThreadingMixIn, ForkingMixIn
 ImportError: cannot import name 'ForkingMixIn'
 >>>
+#http://blog.csdn.net/blueheart20/article/details/60954379
+解决方法
+定位Python的安装目录，查找socketserver.py, 在python_home\Lib下找到了对应的socketserver.py文件，故这个包其实应该是按照好了，所以应该转换方向重新分析问题，于是把焦点重新定位于flask本身，也许是缺少其他类似包吧：
+故经过一番查找，找到类似的问题解决方案：
+>>pip install -U werkzeug
+>> pip install gunicorn
+
+
+
+
+sublime text3中文文件名显示为框框，怎么解决?
+
+点击Preferences选项——settings
+{
+    "font_size": 20,
+    "ignored_packages":
+    [
+        "Vintage"
+    ]
+}
+
+修改为：
+
+{
+    "font_size": 22,
+    "dpi_scale":1.0    //这里是关键中的关键。
+}
