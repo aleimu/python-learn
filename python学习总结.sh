47f164922959ea38384b9667d8ad3afbdb670666
lgj <2086530297@qq.com>
2017-10-05 22:33:38 +0800

    Update python学习总结.sh

--- python学习总结.sh
+++ python学习总结.sh
@@ -6394,6 +6394,35 @@ x,y: 4 None
 3. send包含了next的迭代功能同时也有传送值给y(y = yield x)的功能
 """
 }
+例子4{
+# yield from iterable本质上等于for item in iterable: yield item的缩写版
+def f_wrapper1(f):
+    for g  in f:
+        yield g
+wrap = f_wrapper1(fab3(5))
+for i in wrap:
+    print(i,end=' ')
+ 
+print('\n使用yield from代替for循环')
+def f_wrapper2(f):
+     yield from f#注意此处必须是一个可生成对象
+wrap = f_wrapper2(fab3(5))
+for i in wrap:
+    print(i,end=' ')
+print('\n---------------------')
+ 
+ 
+print('yield from包含多个子程序')
+def g(x):
+    yield from range(x, 0, -1)
+    yield from range(x)
+print(list(g(5)))
+for g  in g(6):
+    print(g,end=',')
+     
+     
+print('\n---------------------')  #yield from iterable本质上等于for item in iterable: yield item的缩写版
+}
 
 }
 
